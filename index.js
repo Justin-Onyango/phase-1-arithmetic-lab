d3d8ce77a01125c0fd404d54dd3b9326d8346174
Justin Onyango <ojustin446@gmail.com>
2024-03-19 11:32:15 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let multiply= 31*2;
+let num1= 32;
+let num2= 2;
+
+let random= Math.floor(Math.random()) +1;
+
+let mod= 10%6;
+let num3= 10;
+let num4= 6;
+
+let max= Math.max(2,5,6,9,12,13,14,16,19,20);
